3dd18253063f0796974099af2016e4aab176e97c
Praniyal Thapa <praniyalthapa100@gmail.com>
2024-12-30 17:07:25 +0545

    pushed some new changes on routes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@ const express=require('express');
 const app=express();
 
 app.use("/hello",(req,res)=>{
-    res.send("Response from the home server");
+    res.send("Response from the server");
     
+})
+app.use('/auth',(req,res)=>{
+    res.send("The authentication of praniyal is done now using this routes")
+
 })
 app.use("/",(req,res)=>{
     res.send("Hello praniyal how are you doing to the new project");
