df8ecf4599f07631411b25aafc574acb99635eff
Praniyal Thapa <praniyalthapa100@gmail.com>
2024-12-31 21:30:01 +0545

    doing some route works

--- src/app.js
+++ src/app.js
@@ -9,26 +9,25 @@ const app=express();
 // app.use("/hello",(req,res)=>{
 //     res.send("Response from the server");
     
-// })
-// app.use('/auth',(req,res)=>{
-//     res.send("The authentication of praniyal is done now using this routes")
-
-// })
-// app.use("/",(req,res)=>{
-//     res.send("Hello praniyal how are you doing to the new project");
-// })
 //To handle different http request we donr use app.use instead we use app.get,post,delete or put
 
-app.post("/user/create",(req,res)=>{
-    res.send("User created separetely now");
-})
-app.get("/user",(req,res)=>{
+// app.post("/user/create",(req,res)=>{
+//     res.send("User created separetely now");
+// })
+// app.get("/user",(req,res)=>{
+//     res.send({
+//         "name":"praniyal thapa",
+//         "occupation":"web tester"
+//     });
+// })
+app.use("/user/:userId",(req,res)=>{
+    console.log(req.params);
     res.send({
         "name":"praniyal thapa",
-        "occupation":"web tester"
-    });
+       "occupation":"web tester"
+     
+    })
 })
-
 
 app.listen(3000,()=>{
     console.log("Server is listening in port 3000...")
