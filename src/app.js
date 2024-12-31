c403e2c4ca43d5c243b1d2714bb0a40a4179ef17
Praniyal Thapa <praniyalthapa100@gmail.com>
2024-12-31 17:51:59 +0545

    added some app.http methods

--- src/app.js
+++ src/app.js
@@ -1,16 +1,32 @@
 const express=require('express');
 const app=express();
 
-app.use("/hello",(req,res)=>{
-    res.send("Response from the server");
-    
-})
-app.use('/auth',(req,res)=>{
-    res.send("The authentication of praniyal is done now using this routes")
 
+
+// app.use("/hello/praniyal",(req,res)=>{
+//     res.send("Hello praniyal you are into /praniyal api request");
+// })
+// app.use("/hello",(req,res)=>{
+//     res.send("Response from the server");
+    
+// })
+// app.use('/auth',(req,res)=>{
+//     res.send("The authentication of praniyal is done now using this routes")
+
+// })
+// app.use("/",(req,res)=>{
+//     res.send("Hello praniyal how are you doing to the new project");
+// })
+//To handle different http request we donr use app.use instead we use app.get,post,delete or put
+
+app.post("/user/create",(req,res)=>{
+    res.send("User created separetely now");
 })
-app.use("/",(req,res)=>{
-    res.send("Hello praniyal how are you doing to the new project");
+app.get("/user",(req,res)=>{
+    res.send({
+        "name":"praniyal thapa",
+        "occupation":"web tester"
+    });
 })
 
 
